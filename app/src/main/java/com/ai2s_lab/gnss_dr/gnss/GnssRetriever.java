8bca10d4598b232013b8b94e0e886fa6bc43f77f
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-01-06 01:54:03 +0900

    [init]

---
+++ app/src/main/java/com/ai2s_lab/gnss_dr/gnss/GnssRetriever.java
@@ -0,0 +1,56 @@
+package com.ai2s_lab.gnss_dr.gnss;
+
+import android.content.Context;
+import android.location.GnssStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+public class GnssRetriever {
+    private static final String TAG = "GNSSRetriever";
+
+    private boolean my_log_data = false;
+    private final LocationManager my_location_manager;
+
+    //Listener for Location data
+    private final LocationListener my_location_listener = new LocationListener() {
+        private static final String TAG = "LocationListener";
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+
+
+            Log.i(TAG, "lat: " + latitude);
+            Log.i(TAG, "long: " + longitude);
+            Log.i(TAG, "alt: " + altitude);
+        }
+    };
+
+    //Listener for GNSS data (satellite info)
+    private final GnssStatus.Callback gnss_status_listener = new GnssStatus.Callback() {
+        private static final String TAG = "GnssStatusCallback";
+
+        @Override
+        public void onFirstFix(int ttffMillis) {
+            super.onFirstFix(ttffMillis);
+        }
+
+        @Override
+        public void onSatelliteStatusChanged(GnssStatus status) {
+            int satCount = status.getSatelliteCount();
+            
+            Log.i(TAG, "satellite count: " + satCount);
+        }
+    };
+
+    public GnssRetriever(Context context) {
+        this.my_location_manager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+}
